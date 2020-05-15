e0db4aac35c1ce1698ddd8f02a3ab1f6e790ee37
YevhenM <marukhnyak.y@gmail.com>
2020-05-16 01:31:49 +0300

    Updates

---
+++ precache-manifest.aac2185d5d0152e9f5c5e4eb2f163ca6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b6d405ef8c4a99b96b0d6809ef9631d",
+    "url": "/YevhenM/s_network_demo.git/index.html"
+  },
+  {
+    "revision": "8f0fc14be6f8aa068ac1",
+    "url": "/YevhenM/s_network_demo.git/static/css/3.145f022a.chunk.css"
+  },
+  {
+    "revision": "22e143af971469bf2b6c",
+    "url": "/YevhenM/s_network_demo.git/static/css/main.3efe2fe1.chunk.css"
+  },
+  {
+    "revision": "85a377c61708a7f55d07",
+    "url": "/YevhenM/s_network_demo.git/static/js/2.85e246b6.chunk.js"
+  },
+  {
+    "revision": "8f0fc14be6f8aa068ac1",
+    "url": "/YevhenM/s_network_demo.git/static/js/3.538af131.chunk.js"
+  },
+  {
+    "revision": "22e143af971469bf2b6c",
+    "url": "/YevhenM/s_network_demo.git/static/js/main.3a815fea.chunk.js"
+  },
+  {
+    "revision": "642df870dd9d9725869a",
+    "url": "/YevhenM/s_network_demo.git/static/js/runtime~main.3c767d5d.js"
+  },
+  {
+    "revision": "7998d46830336c7b3ba398e4e747cf30",
+    "url": "/YevhenM/s_network_demo.git/static/media/maleUser.7998d468.jpg"
+  }
+]);
