0235ea7d655fc92039ddc849d42d26ec75d9762f
YevhenM <marukhnyak.y@gmail.com>
2020-05-22 12:34:36 +0300

    Updates

---
+++ precache-manifest.30a469ead3e2082fdfc23be66052fbd5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47a41eb8682d1d7f85179c8ead6c5f0e",
+    "url": "/s_network_demo/index.html"
+  },
+  {
+    "revision": "fdc348d9717626b16e5d",
+    "url": "/s_network_demo/static/css/3.145f022a.chunk.css"
+  },
+  {
+    "revision": "eb7e4d2251a835498387",
+    "url": "/s_network_demo/static/css/main.afc419f3.chunk.css"
+  },
+  {
+    "revision": "ccd5eca45afa3dd7cb09",
+    "url": "/s_network_demo/static/js/2.d13bae46.chunk.js"
+  },
+  {
+    "revision": "fdc348d9717626b16e5d",
+    "url": "/s_network_demo/static/js/3.8a2db275.chunk.js"
+  },
+  {
+    "revision": "eb7e4d2251a835498387",
+    "url": "/s_network_demo/static/js/main.cf9961c0.chunk.js"
+  },
+  {
+    "revision": "5ed007f72372bd757f4f",
+    "url": "/s_network_demo/static/js/runtime~main.8900b797.js"
+  },
+  {
+    "revision": "7998d46830336c7b3ba398e4e747cf30",
+    "url": "/s_network_demo/static/media/maleUser.7998d468.jpg"
+  }
+]);
